c8b42cc1b4fe0c0be98066f5155fd608fc6d3524
satoruk <koyanagi3106@gmail.com>
2020-08-25 10:11:26 +0900

    feat: add GPG encryption

--- src/inputs.test.ts
+++ src/inputs.test.ts
@@ -2,7 +2,6 @@ import { loadInputs } from "./inputs";
 import { setup, reset, actionWith } from "./testUtils";
 
 beforeEach(() => {
-  jest.resetModules();
   setup();
 });
 afterEach(() => {
@@ -13,8 +12,18 @@ describe("loadInputs", () => {
   test.each([
     [
       "satisfied all params",
-      { "config-file": "dummy.yml" }, //
+      { "config-file": "dummy.yml" },
       { configFile: "dummy.yml" },
+    ],
+    [
+      "satisfied all params with blank gpg-passphrase",
+      { "config-file": "dummy.yml", "gpg-passphrase": "" },
+      { configFile: "dummy.yml" },
+    ],
+    [
+      "satisfied all params with gpg-passphrase",
+      { "config-file": "dummy.yml", "gpg-passphrase": "pass" },
+      { configFile: "dummy.yml", gpgPassphrase: "pass" },
     ],
   ])("loadInputs() with %s", async (_, params, expected) => {
     actionWith(params);
